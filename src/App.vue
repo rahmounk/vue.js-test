deb68127ce39ea2e1d5df595519ff70f98a45510
Karim <rahmoun.karim5@gmail.com>
2021-02-11 18:28:54 +0100

    first commit test vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- TITRE -->
+  <div id="app">
+    <img src="./assets/snk.png" alt="">
+    <h1>SASAGEYO</h1>
+
+
+    <!-- Compteur  -->
+    <div id="counter">
+      Nombre de recrues : {{ counter }}
+    </div>
+
+    <!-- Conditional -->
+    <div id="conditional">
+      <span v-if="clicks>10">Calme-toi stp</span>
+      <div v-if="clicks>50">Mec respire wtf</div>
+    </div>
+
+    <!-- Action au clic -->
+    <button v-on:click="clicked" id="clicks">
+      Clicks: {{ clicks }}
+    </button>
+
+    <!-- Input qui injecte le texte en titre -->
+    <h2>{{ message }}</h2>
+    <input v-model="message"/>
+  </div>
+
+  <!-- ALERTE -->
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data(){
+    return {
+      counter:0,
+      clicks:0,
+      message: "Salut !",
+    }
+  },
+  mounted() {
+    setInterval(() =>{
+      this.counter++
+    },3000)
+  },
+  methods : {
+    clicked(){
+      this.clicks++;
+    }
+  },
+  
 }
 </script>
 
@@ -22,5 +64,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-image: radial-gradient(circle, #ffffff, #efeffb, #dbe0f7, #c4d3f4, #a9c6f0);
 }
 </style>
